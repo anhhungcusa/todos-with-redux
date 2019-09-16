688148511e40fd58e45144f8c0fed7046ef4ec17
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-09-16 13:22:59 +0700

    try add  style for  file README

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-Todo app
-using:
-    + redux
-    + react-redux
-    + react-thunk
+<h1>Todo app</h1> 
+<div>using:</div>
+<ul>
+    <li>redux</li>
+    <li>react-redux</li>
+    <li>react-thunk</li>
+</ul>
--- src/App.js
+++ src/App.js
@@ -6,27 +6,28 @@ import rootReducers from './reducers/rootReducers';
 import {  changeDynamicValue } from './actions/counterActions'
 // import counterReducer from './reducers/counterReducer';
 /* eslint-disable no-underscore-dangle */
-const store = createStore(rootReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(rootReducers, window.STATE_FROM_SERVER, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
  /* eslint-enable */
 
 function App() {
     const [state, setState] = useState(null);
     useEffect(() => {
-        store.subscribe(() => {
-            console.log('subscribe')
-            const { counterReducer } = store.getState();
-            setState(counterReducer);
-        })
+    
     }, [])
     const boundChangeDynamicValue = value => store.dispatch(changeDynamicValue(value));
     const handleClick = (value) => {
             boundChangeDynamicValue(value)
+    }
+    const unsubscribe = React.useCallback(store.subscribe(() => console.log(store.getState())), [])
+    const handleUnsubscribe = () => {
+        unsubscribe();
     }
     return (
         <div className="App">
             Count: {state ? state.count : 0}
             <button onClick={() => handleClick(1)}>increase</button>
             <button onClick={() => handleClick(-2)}>decrease</button>
+            <button onClick={handleUnsubscribe}>unsubscribe</button>
         </div>
     );
 }
